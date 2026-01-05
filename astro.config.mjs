14d1bb2eb2babe430e53464bd999f4ea1877b5a5
unknown <aleksendric@gmail.com>
2026-01-05 13:16:45 +0100

    changed colors and layout cosmetics

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,14 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  image: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**.datocms-assets.com',
+      },
+    ],
+  },
   vite: {
     plugins: [tailwindcss()]
   }
